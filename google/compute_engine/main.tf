5e33023b44e2f88b06d60945261c4e43508c551e
Prog24 <kenta.suzuki6@x-factory.jp>
2026-02-11 18:23:29 +0900

    マシンスペックの変更

--- google/compute_engine/main.tf
+++ google/compute_engine/main.tf
@@ -60,7 +60,7 @@ resource "google_compute_disk" "mc_data_disk" {
 resource "google_compute_instance" "mc_server" {
   project = var.project_id
   name = "mc-server"
-  machine_type = "e2-standard-2"
+  machine_type = "n2-standard-4"
   zone = var.zone
   tags = ["mc-server"]
   metadata_startup_script = templatefile(
